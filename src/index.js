29c045fee9d25d3545b8ec333b10e158eeb89f7a
Viacheslav Tsarenkov <tsarenkovslava@yandex.ru>
2020-10-15 19:51:13 +0300

    feat
    add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let message = expr.split("**********");
+   index = 0;
+   let result = "";
+   for (let i = 0; i < message.length; ++i){
+     let char = "";
+     while(message[i] !== ""){
+       char = "";
+       let letter = message[i].substring(0, 10);
+       message[i] = message[i].replace(letter,"");
+       for (let j = 0; j < letter.length; j += 2){
+         if (letter[j] + letter[j + 1] === "10"){
+           char = char + '.';
+         } else if (letter[j] + letter[j + 1] === "11"){
+          char = char + '-';
+         }
+       }
+       result = result + MORSE_TABLE[char];
+     }
+     result = result + " ";
+   }
+   return result.trim();
 }
 
 module.exports = {
